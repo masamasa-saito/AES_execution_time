e25a534b170678cdaa6dfdd88f2c0265e93b6751
saito_masashi <masasan0319@gmail.com>
2018-12-31 16:52:40 +0900

    newton_method/input/write_input_txt.rbのコメントアウトを修正

--- programming_code/newton_method/input/write_input_txt.rb
+++ programming_code/newton_method/input/write_input_txt.rb
@@ -2,10 +2,9 @@
 #
 # write_input_txt.rb
 #
-# 決められた3次の方程式に対し、
-# 「../input/input.txt」のファイルから一行ずつ数値を読み込み、
-# それらを初期値としたニュートン法で求めて、
-# 全てを解き終わるまでの実行時間を求める
+# 「./input.txt」のファイルに、-10000～19999までの
+#  ランダムな数値を20000個、1行ずつ書き込む
+#
 ####################################################
 
 
@@ -17,16 +16,3 @@ File.open("./input.txt","w+") do |f|
     end
 end
 
-### ここから時間測定開始 ###
-=begin
-execution_time = Benchmark.realtime do
-  input_arr.each do |initial_value| 
-    answer = newton_method( initial_value )
-    puts "answer = #{answer}"
-    puts "==============================="
-  end
-end
-
-
-puts "全ての解を求めるまでにかかった時間：#{execution_time*1000}[ms]"
-=end
